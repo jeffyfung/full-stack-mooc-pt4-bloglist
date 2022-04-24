21ceea3d4912c23555145fb731e63b88d51d051b
Jeffy Fung <jeffyfungkh@gmail.com>
2022-04-24 23:35:26 +0800

    4.21 bloglist expansion step9

--- controllers/blog.js
+++ controllers/blog.js
@@ -25,7 +25,34 @@ router.post('/', async (req, res, next) => {
       user: user.id
     });
     let blogs = await blog.save();
+    user.blogs = user.blogs.concat(blog.id);
+    await user.save();
+
     res.status(201).json(blogs);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.delete('/:id', async (req, res, next) => {
+  try {
+    const decodedToken = jwt.verify(req.token, process.env.SECRET);
+    if (!decodedToken.id) {
+      return res.status(401).json({ error: 'token missing or invalid' });
+    }
+
+    const blog = await Blog.findById(req.params.id);
+    console.log(blog);
+    if (!blog) {
+      return res.status(404).json({ error: 'blog does not exist' });
+    }
+
+    if (blog.user.toString() !== decodedToken.id) {
+      return res.status(401).json({ error: 'no authority to delete this blog' });
+    }
+
+    await Blog.findByIdAndDelete(blog.id);
+    return res.status(204).end();
   } catch (err) {
     next(err);
   }
